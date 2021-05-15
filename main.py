08a72e16aef678e1ebdeefd8e36d9f81df364622
estrap <84222195+estrap@users.noreply.github.com>
2021-05-15 11:41:28 +0200

    elle patch 1
    changed healing intensity, choice question, choice messages.

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 player = {'name': 'Lili', 'attack': 10, 'heal': 12, 'health': 100}
 monster = {'name': 'Bug', 'attack': 12, 'health': 100}
 
-print('Welcome to Monster Game!!')
 print('Welcome to 21sw_Coding class Monster Game!!')
-print('Please select action')
+print('What do you want to do?')
 print('1) Attack')
 print('2) Heal')
 
 player_choice = input()
 
 if player_choice == '1':
-  print('Attack! No mercy !')
+  print('Attack! No mercy!')
   
 elif player_choice == '2':
-  print('Heal player -Huurry!')
+  print('Healing player...')
 
 else:
 
